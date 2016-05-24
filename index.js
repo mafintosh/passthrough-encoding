67430ae7d92c0158394e94e7ca1247293b933dc2
Mathias Buus <mathiasbuus@gmail.com>
2016-05-24 08:35:08 +0000

    support null value

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 exports.encode = function (value, buf, offset) {
+  if (!value) value = Buffer(0)
   if (typeof value === 'string') value = Buffer(value)
+
   exports.encode.bytes = value.length
+
   if (!buf) return value
   if (!offset) offset = 0
-  value.copy(buf, offset)
+  if (value.length) value.copy(buf, offset)
+
   return buf
 }
+
+exports.encode.bytes = 0
 
 exports.decode = function (buf, offset, end) {
   exports.decode.bytes = (end || buf.length) - (offset || 0)
@@ -13,7 +19,10 @@ exports.decode = function (buf, offset, end) {
   return buf
 }
 
+exports.decode.bytes = 0
+
 exports.encodingLength = function (buf) {
+  if (!buf) return 0
   if (typeof buf === 'string') return Buffer.byteLength(buf)
   return buf.length
 }
